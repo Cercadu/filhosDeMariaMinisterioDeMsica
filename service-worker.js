819c5da59c9eefb874c1ea7c1099f1084022e9d0
Cadu Rocha <97856689+Cercadu@users.noreply.github.com>
2025-06-11 10:28:19 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,23 @@
+<llm-snippet-file>service-worker.js</llm-snippet-file>
 // Nome do cache
 const CACHE_NAME = 'cifras-fm-v1';
 
 // Arquivos para cache
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/manifest.json',
-    '/songs.json',
-    '/icons/icon-72x72.png',
-    '/icons/icon-96x96.png',
-    '/icons/icon-128x128.png',
-    '/icons/icon-144x144.png',
-    '/icons/icon-152x152.png',
-    '/icons/icon-192x192.png',
-    '/icons/icon-384x384.png',
-    '/icons/icon-512x512.png',
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './songs.json',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js',
     'https://cdn.jsdelivr.net/npm/vue@3.2.31/dist/vue.global.js',
